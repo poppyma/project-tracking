db644d0e74ea42727539067aee25e5f0ae18b648
poppyma <poppyma05@student.ub.ac.id>
2026-01-28 08:47:16 +0700

    a.ts

--- app/data-master/view-siis/page.tsx
+++ app/data-master/view-siis/page.tsx
@@ -123,7 +123,7 @@ export default function ViewSIISPage() {
     if (!v || v === 0) return "-";
 
     return v
-      .toFixed(3)      // tetap 3 desimal
+      .toFixed(4)      // tetap 3 desimal
       .replace(".", ","); // titik → koma
   }
 
